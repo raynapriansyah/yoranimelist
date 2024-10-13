528770d36d4790f4df7d4b586546d4eb96ed2d13
raynapriansyah <ryan.apriansyah46@gmail.com>
2024-10-13 11:23:46 +0700

    top-anime

--- src/components/topanime.tsx
+++ src/components/topanime.tsx
@@ -4,48 +4,43 @@ import {
   CarouselContent,
   CarouselItem,
 } from "~/components/ui/carousel";
-import { Card, CardContent } from "~/components/ui/card";
 import Image from "next/image";
 
 export default async function TopAnime() {
   async function getAnimeTop() {
-    const response = await fetch("https://api.jikan.moe/v4/top/anime");
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/top/anime`
+    );
     const data = await response.json();
     return data;
   }
 
-  const animes = await getAnimeTop();
+  const anime = await getAnimeTop();
 
   return (
     <div>
-      <Carousel className="w-full">
-        <CarouselContent>
-          {Array.from({ length: 1 }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              // className="basis-1/2 md:basis-1/3 lg:basis-1/4"
-            >
-              <div className="p-1 gap-4 grid grid-cols-4">
-                {animes.data.map((data: any) => {
-                  return (
-                    <Card key={data.mal_id}>
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <Image
-                          src={data.images.jpg.image_url}
-                          alt="image"
-                          width={1280}
-                          height={720}
-                        />
-                      </CardContent>
-                      <div className="text-center">
-                        <h1>{data.title}</h1>
-                      </div>
-                    </Card>
-                  );
-                })}
-              </div>
-            </CarouselItem>
-          ))}
+      <Carousel>
+        <CarouselContent className="p-2">
+          {anime.data.map((data: any) => {
+            return (
+              <CarouselItem
+                key={data.mal_id}
+                className="basis-1/3 md:basis-1/4 lg:basis-1/6"
+              >
+                <div className="mb-4 flex justify-center">
+                  <Image
+                    src={data.images.jpg.image_url}
+                    alt="imageAnime"
+                    width={250}
+                    height={250}
+                  />
+                </div>
+                <div className="text-[12px] md:text-[14px] lg:text-lg text-center justify-center">
+                  <h1>{data.title}</h1>
+                </div>
+              </CarouselItem>
+            );
+          })}
         </CarouselContent>
       </Carousel>
     </div>
