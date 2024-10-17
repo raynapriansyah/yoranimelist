c54bf7ad0bdda9aaa80d474c781c0b50ff955e63
raynapriansyah <ryan.apriansyah46@gmail.com>
2024-10-17 09:08:19 +0700

    manga

--- src/app/top-manga/page.tsx
+++ src/app/top-manga/page.tsx
@@ -4,18 +4,18 @@ import { getMangaTop } from "~/lib/fetch-data";
 import { MangaApiProps } from "~/types/api-props";
 
 export default async function Page() {
-  const anime = await getMangaTop();
+  const manga = await getMangaTop();
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-7 gap-2 md:gap-4">
-      {anime.data.map((manga: MangaApiProps) => {
+      {manga.data.map((manga: MangaApiProps) => {
         return (
           <div key={manga.mal_id}>
             <div className="mt-8 ">
               <div>
                 <Image
                   src={manga.images.jpg.image_url}
-                  alt="imageAnime"
+                  alt="imageManga"
                   width={800}
                   height={800}
                   className="aspect-square rounded-md"
